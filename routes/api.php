1d3955dd8bced416fa403d36db661931923d21ec
Davson Santos <davsonsantos@gmail.com>
2024-02-26 08:23:04 -0400

    me controller

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\User\MeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', LoginController::class);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', [MeController::class, 'show']);
 });
