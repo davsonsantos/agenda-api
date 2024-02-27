5f07dbc29903924bb111a55d84fb2d886f7cdbdf
Davson Santos <davsonsantos@gmail.com>
2024-02-27 10:52:42 -0400

    middleware

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\User\MeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', LoginController::class);
-Route::post('register', RegisterController::class);
+Route::middleware('web')->group(function () {
+    Route::post('login', LoginController::class);
+    Route::post('register', RegisterController::class);
+});
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', [MeController::class, 'show']);
 });
