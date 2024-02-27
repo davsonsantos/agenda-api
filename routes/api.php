a9b14ce2ee124fc16e052aba83a799745e92feac
Davson Santos <davsonsantos@gmail.com>
2024-02-27 07:58:51 -0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', LoginController::class);
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', [MeController::class, 'show']);
 });
